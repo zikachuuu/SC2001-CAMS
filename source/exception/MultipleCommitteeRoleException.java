a397925b02134b7af666ed842d07d69e65019850
tamatamatea <131578122+tamatamatea@users.noreply.github.com>
2023-11-19 00:39:56 +0800

    Update MultipleCommitteeRoleException.java

--- source/exception/MultipleCommitteeRoleException.java
+++ source/exception/MultipleCommitteeRoleException.java
@@ -1,7 +1,18 @@
 package source.exception;
 
+/**
+ * Represents an exception that limits camp committee members to only be the camp committee member for one camp.
+ * @author daryl tan
+ * @version 1
+ * @since 2023-11-19
+ */
 public class MultipleCommitteeRoleException extends RuntimeException {
-    
+
+    /**
+     * Creates a MultipleCommitteeRoleException that prints an error message to Camp Committee member if they were to 
+     * register as a camp committee member for more than one camp 
+     * @param message 
+     */
     public MultipleCommitteeRoleException() {
         super ("Can only be in the camp committee for one camp!") ;
     }
@@ -9,4 +20,3 @@ public class MultipleCommitteeRoleException extends RuntimeException {
     public MultipleCommitteeRoleException(String message) {
         super (message) ;
     }
-}
