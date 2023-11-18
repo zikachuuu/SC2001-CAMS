2f7abeb1ced11a6c486572cb34184d2509f33fbb
tamatamatea <131578122+tamatamatea@users.noreply.github.com>
2023-11-19 00:29:06 +0800

    Update DeadlineOverException.java

--- source/exception/DeadlineOverException.java
+++ source/exception/DeadlineOverException.java
@@ -1,7 +1,17 @@
 package source.exception;
 
+/**
+ * Represents the exception for when a camp registration deadline has passed 
+ * @author florian
+ * @version 1
+ * @since 2023-11-19
+ */
 public class DeadlineOverException extends RuntimeException{
-    
+
+    /**
+     * Create a DeadlineOverException which prints a message when the camp registration deadline has passed 
+     * @param message 
+     */
     public DeadlineOverException() {
         super("Camp registration deadline passed!") ;
     }
