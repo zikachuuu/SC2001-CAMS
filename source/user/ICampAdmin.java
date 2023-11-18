617420017e0fdd75a09d0901c17de06cbf12fd29
zikachuuu <leyanzhi@email.com>
2023-11-18 23:05:18 +0800

    o

--- source/user/ICampAdmin.java
+++ source/user/ICampAdmin.java
@@ -5,11 +5,8 @@ import java.time.LocalDate;
 public interface ICampAdmin {
 
     public boolean createCamp (String campName , LocalDate startDate , LocalDate endDate , LocalDate registrationClosingDate , Faculty userGroup , String location , int totalSlots , int campCommitteeSlots , String description) ;
-    
     public void deleteCamp (String campName) ;
-
     public void editCamp (String campName , LocalDate startDate , LocalDate endDate , LocalDate registrationClosingDate , Faculty userGroup , String location , int totalSlots , int campCommitteeSlots , String description) ;
-
     public void viewCreatedCamps () ;
-    
+    public boolean toggleVisibility(String campName) ;
 }
--- source/user/ICampParticipant.java
+++ source/user/ICampParticipant.java
@@ -2,6 +2,8 @@ package source.user;
 
 public interface ICampParticipant {
     
+    public void viewOpenCamps() ;
+    public void viewRegisteredCamps() ;
     public void registerForCamp (String campName , boolean committeeRole) ;
     public boolean withdrawFromCamp (String campName) ;
 }
