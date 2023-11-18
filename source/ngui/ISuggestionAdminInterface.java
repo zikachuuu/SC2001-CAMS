a6edf7a6ed6e425bd05c0336f720022c94a73dc2
tamatamatea <131578122+tamatamatea@users.noreply.github.com>
2023-11-19 01:39:38 +0800

    Update ISuggestionAdminInterface.java

--- source/ngui/ISuggestionAdminInterface.java
+++ source/ngui/ISuggestionAdminInterface.java
@@ -2,6 +2,16 @@ package source.ngui;
 
 import source.user.User;
 
+/**
+ * Represents an interface for handling suggestions 
+ * @author ranielle chio
+ * @version 1
+ * @since 2023-11-19
+ */
 public interface ISuggestionAdminInterface {
+    /**
+     * Creates an abstract method for handling and approving suggestions 
+     * @param user
+     */
     public void handleSuggestionViewApprove (User user) ;
 }
