fb244a07cf00dacdb9b703e20d01520654d993f7
tamatamatea <131578122+tamatamatea@users.noreply.github.com>
2023-11-19 00:21:23 +0800

    Update CampNotFoundException.java

--- source/exception/CampNotFoundException.java
+++ source/exception/CampNotFoundException.java
@@ -1,6 +1,16 @@
 package source.exception;
 
+/**
+ * Represents exception for when camp is not found 
+ * @author daryltan
+ * @version 1
+ * @since 2023-11-19
+ */
 public class CampNotFoundException extends RuntimeException {
+    /**
+     * Create an exception for when Camp is not found and prints message for user 
+     * @param message 
+     */
     public CampNotFoundException () {
         super ("No such camp with given camp name!") ;
     }
