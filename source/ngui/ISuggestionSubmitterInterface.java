02ee4c4316c1762c17f08041dddfb0e5f0f4e555
tamatamatea <131578122+tamatamatea@users.noreply.github.com>
2023-11-19 01:41:16 +0800

    Update ISuggestionSubmitterInterface.java

--- source/ngui/ISuggestionSubmitterInterface.java
+++ source/ngui/ISuggestionSubmitterInterface.java
@@ -2,6 +2,16 @@ package source.ngui;
 
 import source.user.User;
 
+/**
+ * Represents an interface for submitting suggestions
+ * @author daryl tan
+ * @version 1
+ * @since 2023-11-19
+ */
 public interface ISuggestionSubmitterInterface {
+    /**
+     * Create a method for handling addition of suggestions by users. 
+     * @param user
+     */
     public void handleSuggestionAdd(User user) ;
 }
