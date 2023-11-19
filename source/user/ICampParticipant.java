5cf29a4732e2b04dcdbc6bb9000d948171e38890
zikachuuu <leyanzhi@email.com>
2023-11-19 22:56:50 +0800

    demorgan law

--- source/user/ICampParticipant.java
+++ source/user/ICampParticipant.java
@@ -5,11 +5,6 @@ public interface ICampParticipant {
 
     /**
     * Print out a list of camps that the student can register (only camp information).<p>
-    * This method checks: <p>
-    * 1) camp's user group is NTU or matches student's faculty.<p>
-    * 2) camp's visibility is set to on.<p>
-    * 3) camp is active (not deleted).
-    * @param student The student who wish to view the open camps.
     */
     public void viewOpenCamps() ;
 
@@ -19,25 +14,16 @@ public interface ICampParticipant {
     public void viewRegisteredCamps() ;
 
     /**
-    * Register for a camp. This method calls CampManager.addParticipantToCamp() to do the actual registration. <p>
-    * Corresponding exception will be thrown if there is any error. No exceptions means student is sucessfully registered.
+    * Register for a camp. <p>
     * @param campName Name of the camp.
     * @param committeeRole True for committee, false for attendee.
-    * @throws MultipleCommitteeRoleException
-    * @throws CampNotFoundException 
-    * @throws InvalidUserGroupException
-    * @throws DateClashException
-    * @throws DeadlineOverException 
-    * @throws CampFullException 
-    * @throws WithdrawnException 
     */
     public void registerForCamp (String campName , boolean committeeRole) ;
 
     /**
-    * Withdraw from a camp. This method calls CampManager.removeParticipantFromCamp() to do the actual withdrawal.<p>
+    * Withdraw from a camp.<p>
     * @param campName Name of the camp.
     * @return True if sucessfully withdrawn, false if student is not attending the camp as attendee in the first place.
-    * @throws CampNotFoundException Thrown by Utility.findCampByName()
     */
     public boolean withdrawFromCamp (String campName) ;
 }
