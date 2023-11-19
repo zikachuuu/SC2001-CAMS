f7a4a42489b51b8c0a21d9f95498b2f1256f0b23
tamatamatea <131578122+tamatamatea@users.noreply.github.com>
2023-11-19 13:47:14 +0800

    Update ICampAdminInterface.java

--- source/ngui/ICampAdminInterface.java
+++ source/ngui/ICampAdminInterface.java
@@ -2,9 +2,34 @@ package source.ngui;
 
 import source.user.User;
 
+/**
+ * Represents an interface for camp administration
+ * @author ranielle chio
+ * @version 1
+ * @since 2023-11-19
+ */
 public interface ICampAdminInterface {
+    /**
+     * Create a method for adding a camp
+     * @param user
+     */
     public void handleCampAdd(User user) ;
+
+    /**
+     * Create a method for editing a camp
+     * @param user
+     */
     public void handleCampEdit(User user) ;
+
+    /**
+     * Create a method for toggling camp visibility 
+     * @param user
+     */
     public void handleCampToggle (User user) ;
+
+    /**
+     * Create a method for deleting a camp 
+     * @param user
+     */
     public void handleCampDelete(User user) ;
 }
