32f8eea06b20c05222fe3a23380f51811cbe2f03
MKONG010 <147264339+MKONG010@users.noreply.github.com>
2023-11-22 12:00:28 +0800

    Update CommitteeWithdrawException.java
    javadoc

--- source/exception/CommitteeWithdrawException.java
+++ source/exception/CommitteeWithdrawException.java
@@ -1,6 +1,17 @@
 package source.exception;
 
+
+    /**
+ * Represents exception for when camp committee tries to withdraw from camp 
+ * @author minjie
+ * @version 1
+ * @since 2023-11-19
+ */
 public class CommitteeWithdrawException extends RuntimeException{
+    /**
+     * Create an exception for when Camp Committee tries to withdraw from camp
+     * @param message 
+     */
     public CommitteeWithdrawException() {
         super ("Cannot withdraw as a camp committee!") ;
     }
