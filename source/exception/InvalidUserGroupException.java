d68bed6caffbe112bd3f19120ae4a9236f0f0c42
Cmathriddles <72093752+Cmathriddles@users.noreply.github.com>
2023-11-22 12:31:04 +0800

    Update InvalidUserGroupException.java

--- source/exception/InvalidUserGroupException.java
+++ source/exception/InvalidUserGroupException.java
@@ -10,12 +10,14 @@ public class InvalidUserGroupException extends RuntimeException{
 
     /**
      * Create an InvalidUserGroupException that prints a message to student user that the camp registration has not been opened to them yet.
-     * @param message 
      */
     public InvalidUserGroupException() {
         super("Camp not opened to the student's user group!") ;
     }
-
+    /**
+     * Create an InvalidUserGroupException that prints a message to student user that the camp registration has not been opened to them yet.
+     * @param message 
+     */
     public InvalidUserGroupException(String message) {
         super (message) ;
     }
